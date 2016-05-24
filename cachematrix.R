4bb0a4a0ac5f1c597454542cd1ee6ae2b5acd27a
Archana Ravi <archana.vivek@gmail.com>
2016-05-23 21:56:07 -0700

    Changes to be commited

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program is very similar to the Cache mean program. It first has a function
+## to create a matrix which caches the inverse. 
+## cacheSolve is the function that computes the inverse of the matrix. If the Inverse
+## of the matrix is present it simply retrieves from cache. If not it will compute.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  m<-NULL
+  set<-function(y) {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inverse) m <<-inverse
+  getinv<-function() m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...)
+{
+  m<-x$getinv()
+  if (!is.null(m))
+  {
+    message("getting cahced data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setinv(m)
+  m
 }
